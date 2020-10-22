af587eb717b82c61b7a89e77a911e7bcb5ac4ce7
kavin <kavin@gmail.com>
2020-10-22 18:34:01 +0530

    Angular-model

--- angular-demo/src/app/app-routing.module.ts
+++ angular-demo/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import { HerosComponent } from './heros/heros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+//   {
+//   path:'hero',
+//   component:HerosComponent
+// }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ angular-demo/src/app/comp/comp.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp',
+  templateUrl: './comp.component.html',
+  styleUrls: ['./comp.component.css']
+})
+export class CompComponent implements OnInit {
+  
+
+  data=[] 
+    name=''
+    id=''
+    dob=''
+    address=''
+    city=''
+
+  
+  constructor() { }
+
+  ngOnInit() {
+    add()
+    {
+      this.data.push({
+        name:this.name,
+        id:this.id,
+        dob:this.dob,
+        address:this.address,
+        city:this.city
+      })
+    }
+  }
+
+}
