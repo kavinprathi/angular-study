01621f421dbaaf44776a54a59d2b447264a6d884
kavin <kavin@gmail.com>
2020-10-30 19:42:02 +0530

    creating and editing in angular

--- angular-demo/src/app/crud/creat/creat.component.ts
+++ angular-demo/src/app/crud/creat/creat.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Routes } from '@angular/router';
+import { ActivatedRoute, Router, Routes } from '@angular/router';
 import { ServiceService } from '../service.service';
 
 @Component({
@@ -10,23 +10,66 @@ import { ServiceService } from '../service.service';
 })
 export class CreatComponent implements OnInit {
   form: FormGroup;
+  id;
+  form_id;
+  
+  c_array;
+  // firstname
   constructor(public serviceservices: ServiceService,
-    private router: Routes) { }
+    private router: Router,
+    private routeparams: ActivatedRoute) {
+    // console.log(this.id);
+    this.id = this.routeparams.snapshot.params['path'];
+console.log(this.id)
+    if (typeof this.id != 'undefined') {
+
+      this.load_data();
+
+    }
+  }
+
+
 
   ngOnInit(): void {
-    this.form = new FormGroup({
-      title: new FormControl('', [Validators.required]),
-      body: new FormControl('', Validators.required)
-    })
+    this.form_int();
   }
   get f() {
     return this.form.controls;
   }
   submit() {
-    console.log(this.form.value);
-    this.serviceservices.create(this.form.value).subscribe(res => {
-      console.log('array');
-      this.router.navigateByUrl('crud/list');
+    if(typeof this.id=='undefined'){
+      this.serviceservices.create(this.form.value).subscribe(res => {
+        console.log('array');
+        this.router.navigateByUrl('/crud/list');
+      })
+  
+    }else{
+          this.serviceservices.update(this.id,this.form.value).subscribe(res =>{
+            this.router.navigateByUrl('/crud/list');
+          })
+    }
+      }
+  form_int() {
+    this.form = new FormGroup({
+      firstname: new FormControl('', [Validators.required]),
+      lastname: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required)
     })
+
+  }
+  load_data() {
+    this.serviceservices.find(this.id).subscribe((dat) => {
+      if (dat['code'] == 200) {
+        this.c_array = dat['data']
+        this.getvalue();
+      }
+
+      console.log('form id', this.c_array)
+    })
+
+  }
+  getvalue() {
+    this.form.patchValue({ firstname: this.c_array.firstname, lastname: this.c_array.lastname, password: this.c_array.password, email: this.c_array.email })
   }
 }
--- angular-demo/src/app/crud/crud.module.ts
+++ angular-demo/src/app/crud/crud.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
-import { Route } from '@angular/compiler/src/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewComponent } from './view/view.component';
 import { CreatComponent } from './creat/creat.component';
+// import { ReactiveformModule } from '../reactiveform/reactiveform.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const route : Routes =[
   {path:'', redirectTo:'list', pathMatch:'full'},
   {path:'list' , component:ListComponent},
-  {path:'view/:bala', component:ViewComponent}
+  {path:'view/:bala', component:ViewComponent},
+  {path:'creat' ,component:CreatComponent},
+  {path:'edit/:path',component:CreatComponent}
   
 ]
 
@@ -18,7 +21,8 @@ const route : Routes =[
   declarations: [ListComponent, ViewComponent, CreatComponent],
   imports: [
     CommonModule,
-    
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(route)
   ]
 })
